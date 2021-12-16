113555dda2d72d078dc126e3e25d4a82f79b4115
Jack Linhart <iatenine@gmail.com>
2021-12-16 10:20:12 -0700

    Add flex display and expand cell functionality

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import { Cell } from "./components/Cell";
 
 function App() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        overflow: "scroll",
+      }}
+    >
+      <Cell display={"hidden"} contains={"mine"} />
+      <Cell display={"hidden"} contains={1} />
       <Cell display={"hidden"} />
       <Cell display={"flagged"} />
       <Cell display={"question"} />
       <Cell display={"mine"} />
       <Cell display={4} />
+      <Cell display={"empty"} />
     </div>
   );
 }
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,10 +2,21 @@ import { React, useState } from "react";
 
 export const Cell = (props) => {
   // State includes: Display
-  // Display states: 'hidden', 'flagged', 'question', 'mine', 'number'
+  // Display states: 'hidden', 'flagged', 'question', 'mine', 'number', 'empty'
 
-  const [display] = useState(props.display);
+  const [display, setDisplay] = useState(props.display);
+  const min = 30;
   const height = 2; // Set this from higher in the hierarchy at some point
+
+  const handleOnClick = (e) => {
+    if (display !== "hidden") return;
+    if (props.contains) {
+      console.log("contains " + props.contains);
+      return;
+    }
+    setDisplay("empty");
+    console.log("Empty cell clicked");
+  };
 
   return (
     <div
@@ -16,7 +27,10 @@ export const Cell = (props) => {
         height: `${height}rem`,
         width: `${height}rem`,
         lineHeight: `${height}rem`,
+        minWidth: `${min}px`,
+        minHeight: `${min}px`,
       }}
+      onClick={handleOnClick}
     ></div>
   );
 };
