55692548d5e288aa54115d3ac6a3a6c6a35160c3
Jack Linhart <iatenine@gmail.com>
2021-12-16 14:21:41 -0700

    Cells now detect when adjacent mines

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 import Cell from "./Cell";
 import { useState, useEffect } from "react";
+import { shuffleArray, getNumberedArray } from "../helpers/mineArray";
 
 /*
     Board should hold state for width, height and seed all mines
 */
 
-const shuffle = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-};
-
 export const Board = () => {
   // Board settings
   const [width, setWidth] = useState(10); //Width in cells
   const totalCells = width * width;
-  const totalMines = 40;
+  const totalMines = 10;
   const [mineLocs, setMineLocs] = useState([]); //Array of mine locations
 
   useEffect(() => {
@@ -26,9 +19,9 @@ export const Board = () => {
     for (let i = 0; i < totalMines; i++) {
       randMineLocs[i] = "mine";
     }
-    const shuffledLocs = shuffle(randMineLocs);
-    console.log(randMineLocs);
-    setMineLocs(shuffledLocs);
+    const shuffledLocs = shuffleArray(randMineLocs);
+    const numberedArr = getNumberedArray(shuffledLocs, width, width);
+    setMineLocs(numberedArr);
   }, []);
 
   // Cell settings (always square)
---
+++ src/helpers/mineArray.js
@@ -0,0 +1,79 @@
+export const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+export const getNumberedArray = (arr, width, height) => {
+  // width and height are the dimensions of the array as measured by the number of columns and rows
+  const numberedArray = arr.map((elem, index) => {
+    if (elem === "mine") return "mine";
+    let mineCount = 0;
+    // Yep... No way this will increase time complexity...
+    const adjacents = getAdjacentCellIndices(index, width, arr.length);
+    adjacents.forEach((elem) => {
+      if (arr[elem] === "mine") mineCount++;
+    });
+    return mineCount === 0 ? "" : mineCount;
+  });
+
+  return numberedArray;
+};
+
+function getAdjacentCellIndices(index, width, length) {
+  // Given a cell index in a 1-d array, divided into rows of width, return the indices of the adjacent cells
+  // Determine x, y coordinates of cell
+  const x = index % width;
+  const y = Math.floor(index / width);
+
+  // Get indices of adjacent cells
+  const adjacentCellIndices = [
+    // Lateral adjacents
+    [x - 1, y],
+    [x + 1, y],
+    // Vertical adjacents
+    [x, y - 1],
+    [x, y + 1],
+    // Diagonals
+    [x - 1, y - 1],
+    [x - 1, y + 1],
+    [x + 1, y - 1],
+    [x + 1, y + 1],
+  ];
+
+  // Remove cells that are out of bounds
+  const validAdjacentCells = adjacentCellIndices.filter((elem) => {
+    return (
+      elem[0] >= 0 &&
+      elem[0] < width &&
+      elem[1] >= 0 &&
+      elem[1] < length / width
+    );
+  });
+
+  // Convert adjacent cell indices to 1-d array indices
+  const oneDimensionIndices = validAdjacentCells.map((elem) => {
+    if (elem[1] === 0) return elem[0];
+    return elem[1] * width + elem[0];
+  });
+
+  // Return array of 1-d array indices
+  return oneDimensionIndices;
+}
+
+export const getAdjacentCells = (x, y, width, height) => {
+  const cells = [];
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      if (i === 0 && j === 0) continue;
+      const newX = x + i;
+      const newY = y + j;
+      if (newX >= 0 && newX < width && newY >= 0 && newY < height) {
+        cells.push([newX, newY]);
+      }
+    }
+  }
+  return cells;
+};
